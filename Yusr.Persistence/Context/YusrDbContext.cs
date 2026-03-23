f4b233a25f6b69c10c2cad414361a2c6c89e8f47
Ahmed Bakri <AhmedBkry204@gmail.com>
2026-03-23 09:07:41 +0300

    add- onModelCreating to YusrDbContext

--- Yusr.Persistence/Context/YusrDbContext.cs
+++ Yusr.Persistence/Context/YusrDbContext.cs
@@ -5,10 +5,25 @@ using Yusr.Persistence.Converters;
 
 namespace Yusr.Persistence.Context
 {
-    public partial class YusrDbContext(DbContextOptions<YusrDbContext> options, ITenantService tenantService) : DbContext(options)
+    public class YusrDbContext(DbContextOptions<YusrDbContext> options, ITenantService tenantService) : DbContext(options)
     {
         private readonly ITenantService _tenantService = tenantService;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(YusrDbContext).Assembly);
 
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                if (typeof(BaseTenantEntity).IsAssignableFrom(entityType.ClrType) && !entityType.ClrType.IsAbstract)
+                {
+                    var method = typeof(YusrDbContext)
+                        .GetMethod(nameof(SetTenantQueryFilter), System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                        ?.MakeGenericMethod(entityType.ClrType);
+
+                    method?.Invoke(this, new object[] { modelBuilder });
+                }
+            }
+        }
         protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
         {
             configurationBuilder.Properties<DateTime>().HaveConversion<DateTimeUtcConverter>();
