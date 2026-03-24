79563f34e4190b4e1d354d7d878248a9b388460f
3braa04 <braabormo@gmail.com>
2026-03-24 07:41:20 +0300

    add email DI, and update nuget packages

---
+++ Yusr.Email/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Yusr.Email.Abstractions.Interfaces;
+
+namespace Yusr.Email
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddYusrEmail(this IServiceCollection services)
+        {
+            services.AddSingleton<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
