6a0fb52ad6b1048ee61a812c5131fb766a4dd112
3braa04 <braabormo@gmail.com>
2026-03-24 18:32:12 +0300

    add TenantConfig

---
+++ Yusr.Persistence/Context/Config/TenantConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Yusr.Core.Abstractions.Entities;
+
+namespace Yusr.Persistence.Context.Config
+{
+    public class TenantConfig : IEntityTypeConfiguration<Tenant>
+    {
+        public void Configure(EntityTypeBuilder<Tenant> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.HasOne(x => x.Currency)
+                .WithMany()
+                .HasForeignKey(x => x.CurrencyId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
