1a24439906cff3e8d3cf3d5feafb91cd0cf83032
3braa04 <braabormo@gmail.com>
2026-03-24 06:56:49 +0300

    Add YusrCors

---
+++ Yusr.Api.Abstractions/Extensions/CorsExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Yusr.Bus.Presentation.Api.Extensions
+{
+    public static class CorsExtensions
+    {
+        private static string CorsName = "AllowFrontend";
+
+        public static IServiceCollection AddYusrCors(this IServiceCollection services, string[] origins)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(CorsName, policy =>
+                {
+                    policy.WithOrigins(origins)
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseYusrCors(this IApplicationBuilder app)
+        {
+            return app.UseCors(CorsName);
+        }
+    }
+}
