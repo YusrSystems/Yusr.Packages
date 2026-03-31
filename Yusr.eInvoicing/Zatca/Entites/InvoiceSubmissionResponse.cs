0be19507d4578a99c0c07b2fc3da8628e5cd0a75
Ahmed Bakri <AhmedBkry204@gmail.com>
2026-03-31 18:05:59 +0300

    seperate- zatca api service DTOs to entities folder

---
+++ Yusr.eInvoicing/Zatca/Entites/InvoiceSubmissionResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Yusr.Infrastructure.eInvoicing.Zatca.Services;
+
+namespace Yusr.eInvoicing.Zatca.Entites
+{
+    public class InvoiceSubmissionResponse
+    {
+        public string InvoiceHash { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public List<string> Warnings { get; set; } = new List<string>();
+        public List<InvoiceError> Errors { get; set; } = new List<InvoiceError>();
+    }
+}
---
+++ Yusr.eInvoicing/Zatca/Entites/ZatcaApiRespone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Yusr.eInvoicing.Zatca.Entites
+{
+    public class ZatcaApiRespone
+    {
+        public bool IsValid { get; set; }
+        public HttpStatusCode ResultCode { get; set; }
+        public string ErrorMessage { get; set; } = "";
+        public string WarningMessage { get; set; } = "";
+        public string ClearedInvoice { get; set; } = string.Empty;
+    }
+}
